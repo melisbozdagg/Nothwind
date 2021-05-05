efddecc9fe56f86b4dedb5b4c59a5ec1d776e4f4
melisbozdagg <melis.bozdag@hotmail.com>
2021-05-05 19:12:48 +0300

    v1-start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nothwind';
+  title:string= 'nothwind';
+  user:string ="Melis Bozdağ"
 }
